6fa825a9a5504d00a6e05a17f8dce5ab0c2f1905
Jenova <japancatjack@gmail.com>
2018-08-06 17:54:56 -1000

    Add files via upload

---
+++ js/plugins/CustomDirectorySave.js
@@ -0,0 +1,11 @@
+var path = require('path'),
+    fs = require('fs');
+
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
